b057fce5c1827ec8c3eba3a5b71af614abb3a2b4
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-01-07 19:24:48 +0530

    context api  project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Profile from './components/Profile'
+import Login from './components/login'
+import UserContextProvider from './context/UserContextProvider'
+function App() {
+
+
+  return (
+    <UserContextProvider>
+      <h1>React With Coffee </h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
